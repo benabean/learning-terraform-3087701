0e853bfb5a668bb0ba16bab05b1563e0f14a0071
benabean <68380333+benabean@users.noreply.github.com>
2023-07-04 12:31:36 +0100

    more

--- main.tf
+++ main.tf
@@ -31,9 +31,9 @@ resource "aws_instance" "blog" {
 
 resource "aws_security_group" "blog" {
   name        = "blog"
-  description = "Allow http/s in Allow everything out"
+  description = "Allow http/s in. Allow everything out"
 
-  vpc_id      = "data.aws_vpc.default.id" 
+  vpc_id      = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
